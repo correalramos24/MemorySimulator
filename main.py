a0a2871d266e9ad2905c985c92d07ad560b7e2fc
Victor Correal BSC <victor.correal@bsc.es>
2024-01-08 19:03:30 +0100

    WIP #1

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    pass
+    # Argument parsing
+    # (Required args: mem size (1 KiB byd default, arch bits (32 by default)
+
+    # Add loop of options
+    # read_{bit,byte,half_word,word}
+    # store_{bit, byte, half_word, word}
+    # print_mem
+    # print_mem_range
+    # Parse 0x as HEX and other with numbers
+
+    # Store strings -> feature
+    # Read instructions from .txt with parser  -> next feature
+
+
+if "__name__" == "__main__":
+    main()
+    print("All done")
---
+++ memory.py
@@ -0,0 +1,64 @@
+class Memory:
+    """
+        Memory with load/store to bit, byte and word access.
+        Access to an unaligned memory address lead to an Exception.
+    """
+
+    def __init__(self, mem_size, arch_bits=32):
+        self.__mem__ = [False for _ in range(mem_size)]  # each position contains a bit. Init to False
+        self.arch_bits = arch_bits
+        self.max_int = (2 ** (arch_bits - 1)) - 1
+        self.min_int = -1 * (2 ** (arch_bits - 1))
+
+    # ========================================BIT OPERATIONS===========================================
+    def load_bit(self, addr):
+        return self.__mem__[addr]
+
+    def store_bit(self, addr, value: bool):
+        self.__mem__[addr] = value
+
+    # ========================================BYTE OPERATIONS==========================================
+    def load_byte(self, addr):
+        # TODO: Check for addr alignment
+        return self.__mem__[addr]
+
+    def store_byte(self, addr, value):
+        # TODO: Check for addr alignment
+        # TODO: Check value range
+        self.__mem__[addr] = value
+
+    # ==========================================WORD OPERATIONS========================================
+    def load_word(self, addr):
+        # TODO: Check for addr alignment
+        return self.__mem__[addr]
+
+    def store_word(self, addr, value):
+        # TODO: Check for addr alignment
+        # TODO: Check value range
+        self.__mem__[addr] = value
+
+    # ==========================================VISUAL OPERATIONS=======================================
+
+    def print_memory_bits(self, blocking_bits=1):
+        for addr in range(0, len(self.__mem__), blocking_bits):
+            for value in self.__mem__[addr:addr + blocking_bits]:
+                print(int(value), end="")
+            print()
+
+    # =========================================PRIVATE OPERATIONS=======================================
+    def __check_addr__(self, addr, target_operation=None):
+        if addr > len(self.__mem__):
+            raise Exception("Out of range access")
+
+    def __convert_to_bits__(self, value: int):
+        # Check if it can be represented in the current arch
+
+        # Convert to bit-representation & return
+
+        pass
+
+
+m = Memory(8)
+m.store_bit(0, True)
+m.store_bit(3, True)
+m.print_memory_bits(4)
